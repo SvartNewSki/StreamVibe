e2667572f4f9548863b186fd27768bae7361c367
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-06 16:57:15 +0300

    /index to /main

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-$router->addRoute("GET", "/index", "HomeController@index");
+$router->addRoute("GET", "/main", "HomeController@index");
 $router->addRoute("GET","/register", "UserController@register");
 $router->addRoute("GET","/movies", "MoviesController@movies");
 
