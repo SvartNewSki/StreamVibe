4122312c086a52a7c8be133d0480b8c620eeaeb8
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-06 16:16:50 +0300

    add pages user main movies

---
+++ src/views/layouts/main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- <link rel="stylesheet" href="views/layouts/style.css" /> -->
+    <title>Document</title>
+</head>
+<body>
+    <div class="highblock">
+        <div class="backImage">
+            <!-- <img src="./img/Sub_Container.png" alt=""> -->
+        </div>
+        <div class="logo">
+            <!-- <img class="logo" src="./img/logo-NewSki.png" alt=""> -->
+        </div>
+        
+        <!-- <div class="title">
+        The Best Streaming Experience
+        </div> -->
+    </div>
+</body>
+</html>
