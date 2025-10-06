8110a139f6eade88745ee76f2d34b11af6b6db86
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-06 16:15:41 +0300

    add index

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use app\controllers\UserController;
+use core\models\BaseView;
+use core\controllers\app;
+use core\controllers\Route\Route;
+use core\models\Db;
+use core\Router\Router;
+
+
+require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/config/app.php";
+
+// $db = Db::getInstance();
+// $q = "SELECT * from public.user WHERE username = :username";
+// $res = $db->queryAssociative($q, ['username' => "Amo"]);
+// var_dump($res);
+
+
+
+$router = new Router();
+require_once __DIR__ . '/routes/web.php';
+$router->dispatch($_SERVER["REQUEST_METHOD"],$_SERVER["REQUEST_URI"]);
