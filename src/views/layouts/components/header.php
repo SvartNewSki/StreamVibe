d8c6d5e8e7bdca6fe2d8715d794140a83b179354
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-06 16:16:26 +0300

    add footer and header

---
+++ src/views/layouts/components/header.php
@@ -0,0 +1,8 @@
+<div class="menu">
+            <ul>
+                <li><a href="/index"></a>Home</li>
+                <li><a href="/movies">Movies & Shows</li>
+                <li><a href="/support">Support</li>
+                <li><a href="/subs">Subscriptions</li>
+            </ul>
+        </div>
