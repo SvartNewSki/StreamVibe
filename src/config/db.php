faaf2c7df290c58f7a65edd4d66487797a96eefd
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-06 16:15:07 +0300

    add db

---
+++ src/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+
+return  [
+    'dbname' => 'StreamVibe',
+    'user' => 'FoxGin',
+    'password' => 'Amir1994!',
+    'host' => 'pg',
+    'driver' => 'pdo_pgsql',
+];
+
