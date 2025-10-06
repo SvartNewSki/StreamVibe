82ed092c47a8781bfa98f0ec83a668c8d0fac0b7
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-06 16:57:39 +0300

    uncomment styles and imgs

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -3,21 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- <link rel="stylesheet" href="views/layouts/style.css" /> -->
+      <link rel="stylesheet" href="views/layouts/style.css" />
     <title>Document</title>
 </head>
 <body>
     <div class="highblock">
         <div class="backImage">
-            <!-- <img src="./img/Sub_Container.png" alt=""> -->
+            <img src="./img/Sub_Container.png" alt="">
         </div>
         <div class="logo">
-            <!-- <img class="logo" src="./img/logo-NewSki.png" alt=""> -->
+            <img class="logo" src="./img/logo-NewSki.png" alt="">
         </div>
         
-        <!-- <div class="title">
+        <div class="title">
         The Best Streaming Experience
-        </div> -->
+        </div>
     </div>
 </body>
 </html>
