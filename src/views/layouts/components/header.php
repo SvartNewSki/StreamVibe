7ceb1236780e0bcb809789616fc6bc41a6eec55d
SvartNewSki <makhmudovAmir@gmail.com>
2025-10-08 12:09:09 +0300

    delete tabs

--- src/views/layouts/components/header.php
+++ src/views/layouts/components/header.php
@@ -1,8 +1,8 @@
 <div class="menu">
-            <ul>
-                <li><a href="/main">Home</a></li>
-                <li><a href="/movies">Movies & Shows</a></li>
-                <li><a href="/support">Support</a></li>
-                <li><a href="/subs"> Subscriptions</a></li>
-            </ul>
-        </div>
+    <ul>
+        <li><a href="/main">Home</a></li>
+        <li><a href="/movies">Movies & Shows</a></li>
+        <li><a href="/support">Support</a></li>
+        <li><a href="/subs"> Subscriptions</a></li>
+    </ul>
+</div>
