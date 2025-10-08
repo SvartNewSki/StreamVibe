68d73c63eee93a2270cfe7df1b42e2959df331b1
SvartNewSki <100725918+SvartNewSki@users.noreply.github.com>
2025-10-08 16:43:19 +0300

    add categories block

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -16,11 +16,66 @@
         </div> -->
         
         <div class="title">
-        <span class="headline">The Best Streaming Experience</span>
+        <span class="headline"></span>
         </div>
     </div>
-    <div class="lowerblock">
-
+    <div class="categories_block">
+        <div class="cat_infoBlock">
+            <div class="cat_text">
+                <h2>Explore our wide variety of categories</h2><br>
+                <span class="cat_subtext">Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new</span> 
+            </div>
+            <div class="cat_sliderArrows">
+                arrows
+            </div>
+        </div>
+        <div class="categories_buttons">
+            <div class="action">
+                <div class="button_pics">
+                    <img src="../../img/Image.png" alt="">
+                    <img src="../../img/Image1.png" alt="">
+                    <img src="../../img/Image2.png" alt="">
+                    <img src="../../img/Image3.png" alt="">
+                </div>
+            Action
+            </div>
+            <div class="action">
+                <div class="button_pics">
+                    <img src="../../img/Image.png" alt="">
+                    <img src="../../img/Image1.png" alt="">
+                    <img src="../../img/Image2.png" alt="">
+                    <img src="../../img/Image3.png" alt="">
+                </div>
+                Adventure
+            </div>
+            <div class="action">
+                    <div class="button_pics">
+                    <img src="../../img/Image.png" alt="">
+                    <img src="../../img/Image1.png" alt="">
+                    <img src="../../img/Image2.png" alt="">
+                    <img src="../../img/Image3.png" alt="">
+                </div>
+                Comedy
+            </div>
+                <div class="action">
+                    <div class="button_pics">
+                    <img src="../../img/Image.png" alt="">
+                    <img src="../../img/Image1.png" alt="">
+                    <img src="../../img/Image2.png" alt="">
+                    <img src="../../img/Image3.png" alt="">
+                </div>
+                Drama
+            </div>
+                <div class="action">
+                    <div class="button_pics">
+                    <img src="../../img/Image.png" alt="">
+                    <img src="../../img/Image1.png" alt="">
+                    <img src="../../img/Image2.png" alt="">
+                    <img src="../../img/Image3.png" alt="">
+                </div>
+                Horror
+            </div>
+        </div>   
     </div>
 </body>
 </html>
